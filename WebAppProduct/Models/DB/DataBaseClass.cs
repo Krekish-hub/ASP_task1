5e7b69b7fdfc3de5f4732a0038c9f6de66fadf03
krek <krekish@list.ru>
2024-08-10 04:32:43 +0900

    1

--- WebAppProduct/Models/DB/DataBaseClass.cs
+++ WebAppProduct/Models/DB/DataBaseClass.cs
@@ -5,19 +5,19 @@
         public class Category
         {
             public int CategoryId { get; set; }
-            public string Name { get; set; }
+            public string ?Name { get; set; }
 
-            public ICollection<Product> Products { get; set; }
+            public ICollection<Product> ?Products { get; set; }
         }
 
         public class Product
         {
             public int ProductId { get; set; }
-            public string Name { get; set; }
+            public string ?Name { get; set; }
             public decimal Price { get; set; }
 
             public int CategoryId { get; set; }
-            public Category Category { get; set; }
+            public Category ?Category { get; set; }
         }
     }
 }
